903da066f16403d8d07dfd0ffd574d56c28293aa
premludhanii26-cell <premludhani26@gmail.com>
2026-01-05 20:59:46 +0530

    changes in js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// Select elements from DOM
 const countValue = document.getElementById("count");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const resetBtn = document.getElementById("reset");
 
-// State
 let count = 0;
 
-// Increase count
 increaseBtn.addEventListener("click", function () {
   count++;
   countValue.textContent = count;
 });
 
-// Decrease count
 decreaseBtn.addEventListener("click", function () {
   count--;
   countValue.textContent = count;
 });
 
-// Reset count
 resetBtn.addEventListener("click", function () {
   count = 0;
   countValue.textContent = count;
